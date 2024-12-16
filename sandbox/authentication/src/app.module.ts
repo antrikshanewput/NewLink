9ea573dc63321d7df9d58725ff7837d5ab2d8c1c
priyansh-jain-newput <priyansh.jain@newput.com>
2024-12-17 05:05:49 +0530

    feat(docker): add new services and update package names to newput-newlink

---
+++ sandbox/authentication/src/app.module.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { AuthenticationModule } from '@newlink/authentication';
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthenticationModule.register(
+      {
+        authenticationField: 'phone',
+        registrationFields: ['first_name', 'last_name', 'email', 'phone', 'password', 'username', 'address', 'pincode', 'gender'],
+        roles: ['Admin', 'Editor', 'Viewer'],
+        features: ['Create Post', 'Edit Post', 'View Post'],
+        permissions: [
+          {
+            role: 'Admin',
+            features: ['Create Post', 'Edit Post', 'View Post'],
+          },
+          {
+            role: 'Editor',
+            features: ['Edit Post', 'View Post'],
+          },
+          {
+            role: 'Viewer',
+            features: ['View Post'],
+          },
+        ],
+      },
+      {
+        synchronize: true,
+      }
+    ),
+  ],
+  controllers: [AppController],
+
+})
+export class AppModule { }
