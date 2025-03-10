c379f2096d8a186d9c96d14df8ebb3dbfdd3768d
hemant-newput <hemant@newput.com>
2025-03-10 13:45:51 +0530

    Added sandbox implementation for logger service

---
+++ sandbox/logger/src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('notification-service API Doc')
+    .setDescription('The notification-service API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
