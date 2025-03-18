59b64b846f17f061b4863b969ebd2fc640e6c123
Shravan Bairagi <shravan@newput.com>
2025-03-18 10:12:36 +0530

    DB audit

--- services/logger/src/index.ts
+++ services/logger/src/index.ts
@@ -1,3 +1,7 @@
 export * from './module';
 export * from './logger/interface/logger-config.interface';
 export * from './logger/logger.service';
+export * from './controllers/audit.controller';
+export * from './dto/audit.dto';
+export * from './services/audit.service';
+export * from './entities/audit.entity';
