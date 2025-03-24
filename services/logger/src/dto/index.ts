51a5ca4e58a642f98d6d82d3985e69b3e69ba3cc
shravanbairagi8 <67500444+shravanbairagi8@users.noreply.github.com>
2025-03-24 11:05:32 +0530

    Revert "DB audit"

--- services/logger/src/dto/index.ts
+++
@@ -1,8 +0,0 @@
-import { AuditDto } from 'dto/audit.dto';
-
-export const DefaultDTO = [
-	{
-		provide: 'AUDIT_DTO',
-		useValue: AuditDto,
-	},
-];
--- services/logger/src/index.ts
+++ services/logger/src/index.ts
@@ -1,7 +1,3 @@
 export * from './module';
 export * from './logger/interface/logger-config.interface';
 export * from './logger/logger.service';
-export * from './controllers/audit.controller';
-export * from './dto/audit.dto';
-export * from './services/audit.service';
-export * from './entities/audit.entity';
